ba7f065f0f6eb8e8a6576e75c7fd101f0577c496
Reed <37314703+MrSamples@users.noreply.github.com>
2023-07-28 16:56:28 -0500

    init commit

---
+++ VarioLoad/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace VarioLoad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.setupBox = new System.Windows.Forms.GroupBox();
+            this.start = new System.Windows.Forms.Button();
+            this.browse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.filePathBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comPortBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.working = new System.Windows.Forms.Label();
+            this.setupBox.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // setupBox
+            // 
+            this.setupBox.Controls.Add(this.working);
+            this.setupBox.Controls.Add(this.start);
+            this.setupBox.Controls.Add(this.browse);
+            this.setupBox.Controls.Add(this.label2);
+            this.setupBox.Controls.Add(this.filePathBox);
+            this.setupBox.Controls.Add(this.label1);
+            this.setupBox.Controls.Add(this.comPortBox);
+            this.setupBox.Location = new System.Drawing.Point(12, 27);
+            this.setupBox.Name = "setupBox";
+            this.setupBox.Size = new System.Drawing.Size(349, 105);
+            this.setupBox.TabIndex = 0;
+            this.setupBox.TabStop = false;
+            this.setupBox.Text = "Setup";
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.start.Location = new System.Drawing.Point(274, 71);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(69, 23);
+            this.start.TabIndex = 7;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(274, 30);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(69, 23);
+            this.browse.TabIndex = 2;
+            this.browse.Text = "Browse...";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Log File";
+            // 
+            // filePathBox
+            // 
+            this.filePathBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.filePathBox.Location = new System.Drawing.Point(6, 32);
+            this.filePathBox.Name = "filePathBox";
+            this.filePathBox.ReadOnly = true;
+            this.filePathBox.Size = new System.Drawing.Size(262, 20);
+            this.filePathBox.TabIndex = 1;
+            this.filePathBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM Port";
+            // 
+            // comPortBox
+            // 
+            this.comPortBox.Location = new System.Drawing.Point(6, 71);
+            this.comPortBox.Name = "comPortBox";
+            this.comPortBox.Size = new System.Drawing.Size(100, 20);
+            this.comPortBox.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.CheckFileExists = false;
+            this.openFileDialog1.DefaultExt = "txt";
+            this.openFileDialog1.FileName = "BatteryTest";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DiscardNull = true;
+            this.serialPort1.DtrEnable = true;
+            this.serialPort1.ReadTimeout = 300;
+            this.serialPort1.ReceivedBytesThreshold = 1024;
+            this.serialPort1.RtsEnable = true;
+            this.serialPort1.ErrorReceived += new System.IO.Ports.SerialErrorReceivedEventHandler(this.serialPort1_ErrorReceived);
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(374, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // working
+            // 
+            this.working.AutoSize = true;
+            this.working.Location = new System.Drawing.Point(112, 74);
+            this.working.Name = "working";
+            this.working.Size = new System.Drawing.Size(56, 13);
+            this.working.TabIndex = 8;
+            this.working.Text = "Working...";
+            this.working.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 143);
+            this.Controls.Add(this.setupBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Variable Load Data Storage v1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.setupBox.ResumeLayout(false);
+            this.setupBox.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox setupBox;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox filePathBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox comPortBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label working;
+    }
+}
+
---
+++ VarioLoad/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace VarioLoad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        TextWriter tw;
+        DateTime curTime;
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "csv files (*.csv)|*.csv|txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            curTime = DateTime.Now;
+            openFileDialog1.FileName = "BatteryTest_" +
+                                       curTime.Year.ToString("D4") + "_" +
+                                       curTime.Month.ToString("D2") + "_" +
+                                       curTime.Day.ToString("D2") + "_" +
+                                       curTime.Hour.ToString("D2") +
+                                       curTime.Minute.ToString("D2") +
+                                       curTime.Second.ToString("D2");
+            SendKeys.Send("{HOME}+{END}");
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tw = new StreamWriter(openFileDialog1.FileName, true);
+                filePathBox.Text = openFileDialog1.FileName;
+                filePathBox.BackColor = Color.GreenYellow;
+            }
+        }
+
+        bool toggle = false, inp1 = true, once = false;
+        float data1, data2;
+        int time = 0;
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string input;
+            byte[] buffer;
+
+            try
+            {
+                buffer = new byte[4];
+                serialPort1.Read(buffer, 0, 4);
+                input = Encoding.UTF8.GetString(buffer);
+                if (input.Contains("\"")) {
+                    toggle = false;
+                    tw.Flush();
+                    tw.Close();
+                    tw.Dispose();
+                    tw = null;
+                    serialPort1.Close();
+                    working.Visible = false;
+                    MessageBox.Show("Done storing data", "Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                if (toggle) {
+                    if (inp1)
+                    {
+                        inp1 = false;
+                        data1 = Convert.ToInt16(input, 16) / 1000.0f;
+                    }
+                    else
+                    {
+                        inp1 = true;
+                        data2 = Convert.ToInt16(input, 16) / 1000.0f;
+                        if (once) {
+                            data1 = data2;
+                            once = false;
+                        }
+                        tw.WriteLine(time.ToString() + "," + data1.ToString() + "," + data2.ToString());
+                        tw.Flush();
+                        time += 600;
+                    }
+                }
+                if (input.Contains("'"))
+                {
+                    toggle = true;
+                    once = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (tw != null)
+                {
+                    tw.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            comPortBox.Text = comPortBox.Text.ToUpper().Trim();
+            if (comPortBox.TextLength == 0)
+                return;
+            if (!comPortBox.Text.StartsWith("COM"))
+                comPortBox.Text = "COM" + comPortBox.Text;
+            try
+            {
+                serialPort1.PortName = comPortBox.Text;
+                serialPort1.BaudRate = 57600;
+                serialPort1.Encoding = Encoding.UTF8;
+                serialPort1.Open();
+                working.Visible = true;
+                if (tw != null)
+                {
+                    tw.WriteLine("Time (s),Min (V),Max (V)");
+                    tw.Flush();
+                }
+                serialPort1.WriteLine(",");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Could Not Open Serial Port", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+            if (tw != null)
+            {
+                tw.Flush();
+                tw.Close();
+            }
+        }
+
+        private void serialPort1_ErrorReceived(object sender, System.IO.Ports.SerialErrorReceivedEventArgs e)
+        {
+            //MessageBox.Show(e.ToString());
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string help = "This application connects to the serial port enumerated by the SparkFun Variable Load board and puts the logged data into a stored text file for user use.\n\n";
+            help += "To run:\n";
+            help += "1. Create the log file.\n";
+            help += "3. Select the COM port number.\n";
+            help += "4. Press the start button and wait for the \"done\" message.\n";
+
+            MessageBox.Show(help, "Help", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
